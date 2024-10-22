80e5e668d26f9dbd221584080057642fcefa25af
QwertyAvatar <dikdikdik@wp.pl>
2024-10-21 19:50:25 -0700

    Initial Commit

---
+++ Kola/KolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KolaApp.swift
+//  Kola
+//
+//  Created by Marcin on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct KolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
